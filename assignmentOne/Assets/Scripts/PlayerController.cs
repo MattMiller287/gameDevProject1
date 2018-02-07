c67ab23f08cc15399863520880c9f5d6cf97510f
MattMiller287 <mattmiller287@gmail.com>
2018-02-06 19:40:08 -0800

    RollABall game 1 complete

---
+++ assignmentOne/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody rb;
+	private int count;
+	public float speed;
+	public Text countText;
+	public Text winText;
+
+	void Start()
+	{
+		count = 0;
+		SetCountText ();
+		rb = GetComponent<Rigidbody> ();
+		winText.text = "";
+	}
+
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0, moveVertical);
+
+		rb.AddForce (movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag ("Pick Up")) 
+		{
+			other.gameObject.SetActive (false);
+			count++;
+			SetCountText ();
+		}
+	}
+		
+	void SetCountText()
+	{
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 12) 
+		{
+			winText.text = "You Win!";	
+		}
+	}
+
+}
